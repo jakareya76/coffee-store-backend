0f9056a9e0f2a5c0f3dde9eba1cf9e9ce171e080
jakareya76 <jakareya1306@gmail.com>
2024-04-22 15:06:40 +0600

    create api end point for create and read data

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+
+    app.get("/coffee", async (req, res) => {
+      const cursur = coffeeCollection.find();
+      const coffee = await cursur.toArray();
+
+      res.send(coffee);
+    });
+
+    app.post("/add-coffee", async (req, res) => {
+      const newCoffee = req.body;
+
+      const result = await coffeeCollection.insertOne(newCoffee);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
