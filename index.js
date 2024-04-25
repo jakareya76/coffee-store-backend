2148ee4a8dd16b7036f361097e67c3e0f4e13560
jakareya76 <jakareya1306@gmail.com>
2024-04-25 16:02:35 +0600

    did some small changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.50cb8a0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -114,9 +114,7 @@ async function run() {
     app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-
       const result = await usersCollection.deleteOne(query);
-
       res.send(result);
     });
 
@@ -125,12 +123,13 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } catch (err) {
-    console.log(err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 
-run();
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
